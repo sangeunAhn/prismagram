719d012aea0a33050a6ec60bafe974c1d93ded8f
sang_eun <angineer01@naver.com>
2020-01-09 11:55:36 +0900

    0109_A1

---
+++ src/server.js
@@ -0,0 +1,14 @@
+import dotenv from 'dotenv';
+import path from 'path';
+dotenv.config({ path: path.resolve(__dirname, '.env') });
+import { GraphQLServer } from 'graphql-yoga';
+import logger from 'morgan';
+import schema from './schema';
+
+const PORT = process.env.PORT || 4000;
+
+const server = new GraphQLServer({ schema });
+
+server.express.use(logger('dev'));
+
+server.start({ port: PORT }, () => console.log(`✅ Server running on port ${PORT}`));
