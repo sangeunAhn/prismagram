64be1d08f33ca14a4543908db5a0c985b3c6ba7c
sang_eun <angineer01@naver.com>
2020-01-10 18:13:26 +0900

    see profile, photo done

--- src/server.js
+++ src/server.js
@@ -1,16 +1,16 @@
-  
-import "./env";
+  import "./env";
 import { GraphQLServer } from "graphql-yoga";
 import logger from "morgan";
 import schema from "./schema";
 import "./passport";
 import { authenticateJwt } from "./passport";
+import { isAuthenticated } from "./middlewares";
 
 const PORT = process.env.PORT || 4000;
 
 const server = new GraphQLServer({
   schema,
-  context: ({ request }) => ({ request })
+  context: ({ request }) => ({ request, isAuthenticated })
 });
 
 server.express.use(logger("dev"));
